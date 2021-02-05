91562298d6a4f40d1498ccd26c82e2df2c78507b
AyoubGrioui <griouiayoub01@gmail.com>
2021-02-05 23:52:12 +0100

    add Lists controllers

--- src/com/sga/controllers/ListeDesDonateurServlet.java
+++ src/com/sga/controllers/ListeDesDonateurServlet.java
@@ -1,11 +1,20 @@
 package com.sga.controllers;
 
+import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import com.sga.entities.Donneur;
+import com.sga.entities.DonneurMoral;
+import com.sga.entities.DonneurPhysique;
+import com.sga.repositories.HibernateDonneurPersister;
+import com.sga.repositories.Repository;
+import com.sga.repositories.RepositoryFactory;
 
 /**
  * Servlet implementation class DashBoardDonateurServlet
@@ -14,14 +23,20 @@ import java.io.IOException;
 public class ListeDesDonateurServlet extends HttpServlet {
 
     /**
-     * 
+     *
      */
     private static final long  serialVersionUID       = 1L;
     public static final String VUE_LISTE_DES_DONATEUR = "/WEB-INF/listeDesDonateurs.jsp";
+    public static final String ATT_DONATEURSLIST="donateurList";
 
     protected void doGet( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
 
+        HibernateDonneurPersister donneurPersister = new HibernateDonneurPersister();
+        List<Donneur> donneurList = donneurPersister.getAll();
+
+
+        request.setAttribute(ATT_DONATEURSLIST, donneurList);
 
         this.getServletContext().getRequestDispatcher( VUE_LISTE_DES_DONATEUR ).forward( request, response );
     }
